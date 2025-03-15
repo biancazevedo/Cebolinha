6594e0c3560eb9ba417753175f9c393488a95b46
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-14 21:33:23 -0300

    acabouuu

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string fraseDigitada;
+
+Console.Write("Digite uma flase: ");
+fraseDigitada = Console.ReadLine()!
+    .Replace("r", "l")
+    .Replace("R", "L");
+
+Console.WriteLine($"{fraseDigitada}");
